1410dc0ef3ee9764327eca26c5dc7369dd29195f
Fenil Kanjiya <70877864+Fenilkanjiya@users.noreply.github.com>
2023-11-20 17:19:38 +0000

    variable done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 15654
+let accountEmail = 'fenil@gmail.com'
+var accountPassword = '951654'
+accountCity = 'ahm'
+let accountState;
+
+// accountId = 9  // not allowd
+
+console.log(accountId)
+
+accountEmail = 'qwe@gmail.com'
+accountPassword = '65a4sda'
+accountCity = 'junagadh'
+
+console.table([accountEmail, accountPassword, accountCity, accountId, accountState])
