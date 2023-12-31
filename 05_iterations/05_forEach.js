253ae78164b5ca039457c723c2aff0687b7c2532
Fenilkanjiya <fenilkanjiya1611@gmail.com>
2023-12-31 13:22:00 +0530

    forEach and forin

---
+++ 05_iterations/05_forEach.js
@@ -0,0 +1,38 @@
+const coding = ["js", "ruby", "java", "python", "cpp"];
+
+// coding.forEach(function(items){
+//     console.log(items)
+// })
+
+// coding.forEach((items) => {
+//     console.log(items)
+// })
+
+// function printMe(items){
+//     console.log(items)
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach((items, index, arr) => {
+//     console.log(items, index, arr)
+// })
+
+const myCoding = [
+  {
+    languageName: "javascript",
+    languageFileName: "js",
+  },
+  {
+    languageName: "java",
+    languageFileName: "java",
+  },
+  {
+    languageName: "python",
+    languageFileName: "py",
+  },
+];
+
+myCoding.forEach((items) => console.log(items.languageFileName));
+
+// console.log(myCoding);
